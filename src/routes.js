c1f7ed71a4958f3c2691df27be60c0dfb47c97ee
Enderson Lúcio Garcia <endersonluciog@gmail.com>
2020-07-21 20:55:27 -0300

    Configurado as paginas,rotas,eslint e prettier

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Detail from './pages/Detail';
+import List from './pages/List';
+import Register from './pages/Register';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="List" component={List} />
+        <AppStack.Screen name="Detail" component={Detail} />
+        <AppStack.Screen name="Register" component={Register} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
